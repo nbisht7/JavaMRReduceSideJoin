d5eeea2ac7fdf808a438c06a0c9822b811ec5647
Neha-Bisht3 <Neha.Bisht3@ibm.com>
2017-11-13 14:01:01 -0800

    Added mapreduce code, jar and input-output files

---
+++ src/main/java/com/nbisht/mr/reducesidejoin/SalesAggregatorReducer.java
@@ -0,0 +1,29 @@
+package com.nbisht.mr.reducesidejoin;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * @author nbisht7. 
+ * Purpose - States Mapper to fetch the data from intermediate directory in hadoop. 
+ * Input Schema - state, salesPrice
+ * Output Schema - state, totalSalesPrice
+*/
+
+public class SalesAggregatorReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+			throws IOException, InterruptedException {
+		double totalSalesPrice = 0.0;
+		for (DoubleWritable val : values) {
+			totalSalesPrice = totalSalesPrice + Double.parseDouble(val.toString());
+		}
+		DoubleWritable totalSales = new DoubleWritable(totalSalesPrice);
+
+		// Validating the data before writing
+		if (!(key.toString().isEmpty()) && !(totalSales == null))
+			context.write(new Text(key), totalSales);
+	}
+}
